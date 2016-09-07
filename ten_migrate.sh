d7273563d456b74af42cd9404a0eb5ab2f852f18
Adarnof <adarnof@gmail.com>
2016-09-06 20:32:30 -0400

    Script for automatic database conversion  - fakes initial migrations to preserve data Include LOGIN_URL setting

---
+++ ten_migrate.sh
@@ -0,0 +1,21 @@
+python manage.py migrate --fake admin 0001_initial
+python manage.py migrate --fake djcelery 0001_initial
+python manage.py migrate --fake sessions 0001_initial
+python manage.py migrate --fake auth 0001_initial
+python manage.py migrate admin
+python manage.py migrate sessions
+python manage.py mgirate djcelery
+python manage.py migrate auth
+python manage.py migrate --fake authentication 0001_initial
+python manage.py migrate --fake celerytask 0001_initial
+python manage.py migrate --fake eveonline 0001_initial
+python manage.py migrate --fake fleetactivitytracking 0001_initial
+python manage.py migrate --fake groupmanagement 0001_initial
+python manage.py migrate --fake hrapplications 0001_initial
+python manage.py migrate --fake notifications 0001_initial
+python manage.py migrate --fake optimer 0001_initial
+python manage.py migrate --fake services 0001_initial
+python manage.py migrate --fake sigtracker 0001_initial
+python manage.py migrate --fake srp 0001_initial
+python manage.py migrate --fake timerboard 0001_initial
+python manage.py migrate
